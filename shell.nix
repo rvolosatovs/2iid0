61631de7570e9994e5cc1db1a87c8cf3756502f6
Roman Volosatovs <rvolosatovs@riseup.net>
2017-12-09 11:23:58 +0100

    wip

--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
-{ nixpkgs ? import <nixpkgs> {}}:
+{ nixpkgs ? import <nixpkgs-unstable> {}}:
 let
   inherit (nixpkgs) pkgs;
   python = pkgs.python36.withPackages (ps: with ps; [
@@ -7,9 +7,13 @@ let
     pandas
     numpy
     matplotlib
+    graphviz
   ]);
 in
   pkgs.stdenv.mkDerivation {
     name = "jupyter-env";
-    buildInputs = [ python pkgs.python36Packages.jupyter ];
+    buildInputs = [ 
+      python 
+      pkgs.python36Packages.jupyter 
+    ];
   }
