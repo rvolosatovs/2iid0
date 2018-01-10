7d12d2823cc9f9b73b60298a80a3ddb0e2cab84b
Roman Volosatovs <rvolosatovs@riseup.net>
2018-01-10 19:59:35 +0100

    fourth: start

---
+++ fourth/PageRankCalc.py
@@ -0,0 +1,17 @@
+import networkx as nx
+
+# load the edge list and create a directed Graph
+fh = open("hamster.edgelist", 'rb')
+G = nx.read_edgelist(fh,create_using=nx.DiGraph())
+fh.close()
+
+# calculate PageRank directly use
+# the function from NetworkX
+# more details about this function
+# can be found 
+# https://networkx.github.io/documentation/latest/reference/algorithms/generated/networkx.algorithms.link_analysis.pagerank_alg.pagerank.html
+pr = nx.pagerank(G, alpha=0.85)
+# the return value pr is dict in Python
+# in whici the keys are the node indices 
+# and the values are corresponding PagRank values
+print pr
---
+++ fourth/requirements.nix
@@ -0,0 +1,141 @@
+# generated using pypi2nix tool (version: 1.8.1)
+# See more at: https://github.com/garbas/pypi2nix
+#
+# COMMAND:
+#   pypi2nix -V 3.6 -e python-louvain
+#
+
+{ pkgs ? import <nixpkgs> {}
+}:
+
+let
+
+  inherit (pkgs) makeWrapper;
+  inherit (pkgs.stdenv.lib) fix' extends inNixShell;
+
+  pythonPackages =
+  import "${toString pkgs.path}/pkgs/top-level/python-packages.nix" {
+    inherit pkgs;
+    inherit (pkgs) stdenv;
+    python = pkgs.python36;
+    # patching pip so it does not try to remove files when running nix-shell
+    overrides =
+      self: super: {
+        bootstrapped-pip = super.bootstrapped-pip.overrideDerivation (old: {
+          patchPhase = old.patchPhase + ''
+            sed -i               -e "s|paths_to_remove.remove(auto_confirm)|#paths_to_remove.remove(auto_confirm)|"                -e "s|self.uninstalled = paths_to_remove|#self.uninstalled = paths_to_remove|"                  $out/${pkgs.python35.sitePackages}/pip/req/req_install.py
+          '';
+        });
+      };
+  };
+
+  commonBuildInputs = [];
+  commonDoCheck = false;
+
+  withPackages = pkgs':
+    let
+      pkgs = builtins.removeAttrs pkgs' ["__unfix__"];
+      interpreter = pythonPackages.buildPythonPackage {
+        name = "python36-interpreter";
+        buildInputs = [ makeWrapper ] ++ (builtins.attrValues pkgs);
+        buildCommand = ''
+          mkdir -p $out/bin
+          ln -s ${pythonPackages.python.interpreter}               $out/bin/${pythonPackages.python.executable}
+          for dep in ${builtins.concatStringsSep " "               (builtins.attrValues pkgs)}; do
+            if [ -d "$dep/bin" ]; then
+              for prog in "$dep/bin/"*; do
+                if [ -f $prog ]; then
+                  ln -s $prog $out/bin/`basename $prog`
+                fi
+              done
+            fi
+          done
+          for prog in "$out/bin/"*; do
+            wrapProgram "$prog" --prefix PYTHONPATH : "$PYTHONPATH"
+          done
+          pushd $out/bin
+          ln -s ${pythonPackages.python.executable} python
+          ln -s ${pythonPackages.python.executable}               python3
+          popd
+        '';
+        passthru.interpreter = pythonPackages.python;
+      };
+    in {
+      __old = pythonPackages;
+      inherit interpreter;
+      mkDerivation = pythonPackages.buildPythonPackage;
+      packages = pkgs;
+      overrideDerivation = drv: f:
+        pythonPackages.buildPythonPackage (drv.drvAttrs // f drv.drvAttrs //                                            { meta = drv.meta; });
+      withPackages = pkgs'':
+        withPackages (pkgs // pkgs'');
+    };
+
+  python = withPackages {};
+
+  generated = self: {
+
+    "decorator" = python.mkDerivation {
+      name = "decorator-4.1.2";
+      src = pkgs.fetchurl { url = "https://pypi.python.org/packages/bb/e0/f6e41e9091e130bf16d4437dabbac3993908e4d6485ecbc985ef1352db94/decorator-4.1.2.tar.gz"; sha256 = "7cb64d38cb8002971710c8899fbdfb859a23a364b7c99dab19d1f719c2ba16b5"; };
+      doCheck = commonDoCheck;
+      buildInputs = commonBuildInputs;
+      propagatedBuildInputs = [ ];
+      meta = with pkgs.stdenv.lib; {
+        homepage = "https://github.com/micheles/decorator";
+        license = licenses.bsdOriginal;
+        description = "Better living through Python with decorators";
+      };
+    };
+
+
+
+    "networkx" = python.mkDerivation {
+      name = "networkx-2.0";
+      src = pkgs.fetchurl { url = "https://pypi.python.org/packages/73/58/0add7d81cf64958f7a062aa287237364eb0a0959bf7a817f708d5c25d043/networkx-2.0.zip"; sha256 = "cd5ff8f75d92c79237f067e2f0876824645d37f017cfffa5b7c9678cae1454aa"; };
+      doCheck = commonDoCheck;
+      buildInputs = commonBuildInputs;
+      propagatedBuildInputs = [
+      self."decorator"
+    ];
+      meta = with pkgs.stdenv.lib; {
+        homepage = "http://networkx.github.io/";
+        license = licenses.bsdOriginal;
+        description = "Python package for creating and manipulating graphs and networks";
+      };
+    };
+
+
+
+    "python-louvain" = python.mkDerivation {
+      name = "python-louvain-0.9";
+      src = pkgs.fetchurl { url = "https://pypi.python.org/packages/a6/77/77b779f336e6a689c97ee3a5181501c94496166ca736c1783b01b7a68ee3/python-louvain-0.9.tar.gz"; sha256 = "82fad0243f4e923801fcfc4b83ca4f742f5bdb4f99abbdc673c7c34ea6fd2885"; };
+      doCheck = commonDoCheck;
+      buildInputs = commonBuildInputs;
+      propagatedBuildInputs = [
+      self."networkx"
+    ];
+      meta = with pkgs.stdenv.lib; {
+        homepage = "https://github.com/taynaud/python-louvain";
+        license = licenses.bsdOriginal;
+        description = "Louvain algorithm for community detection";
+      };
+    };
+
+  };
+  localOverridesFile = ./requirements_override.nix;
+  overrides = import localOverridesFile { inherit pkgs python; };
+  commonOverrides = [
+
+  ];
+  allOverrides =
+    (if (builtins.pathExists localOverridesFile)
+     then [overrides] else [] ) ++ commonOverrides;
+
+in python.withPackages
+   (fix' (pkgs.lib.fold
+            extends
+            generated
+            allOverrides
+         )
+   )
